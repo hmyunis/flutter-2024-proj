5f672cb1a136a3bb32edc32dceca3f896e518dba
ldmoti <saltyanalysis89@gmail.com>
2024-05-27 10:08:15 +0300

    Removed dummy data.

--- lib/presentation/data/accounts.dart
+++
@@ -1,8 +0,0 @@
-import '../../models/account.dart';
-
-List<Account> accounts = const [
-  Account(userName: "Estifanos", userType: "Admin"),
-  Account(userName: "Hamdi", userType: "User"),
-  Account(userName: "Bela", userType: "Owner"),
-  Account(userName: "Moti", userType: "Admin"),
-];
